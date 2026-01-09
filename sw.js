eeaa4a7c615c1123973e2cc25f904f2db93f2100
Guaiapp <joru079@gmail.com>
2026-01-09 09:55:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = "guaiapp-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./logo-guaiapp.png"
-];
-
-// Instalación: cache mínimo
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
-// Activación
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-// Fetch: primero cache, luego red
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  // Passthrough: deja que el navegador maneje la red
 });
