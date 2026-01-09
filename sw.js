5462f510699d2566fa1d9a4d42e57d160e4997ac
Guaiapp <joru079@gmail.com>
2026-01-08 21:44:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,30 +7,26 @@ const FILES_TO_CACHE = [
   "./logo-guaiapp.png"
 ];
 
+// Instalación: cache mínimo
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// Activación
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
+// Fetch: primero cache, luego red
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
