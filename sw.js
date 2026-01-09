769b956d0fcffc4e361f7b1400795ed39a44e766
Guaiapp <joru079@gmail.com>
2026-01-09 11:52:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,21 @@
-const CACHE_NAME = "guaiapp-v2";
-
-const FILES_TO_CACHE = [
-  "/guaiapp/",
-  "/guaiapp/index.html",
-  "/guaiapp/manifest.json",
-  "/guaiapp/logotipo-guaiapp.png"
+const CACHE_NAME = 'guaiapp-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logotipo-guaiapp.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
